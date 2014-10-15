1787bb6606d98721456d5ecc1629f901d3395351
Andreas Drotth <drotth@hotmail.com>
2014-10-15 23:55:55 +0200

    Cleanup and bug found!
    Cleaned and structured a bit of the code in lab2, preparing it for
    demonstration. Also found that the Reduce/GCD functions doesn't seem
    to be working correctly. A rational of 2/4 should be turned into
    1/2 but that doesn't seem to happen.

--- lab2/lab2/Rational.h
+++ lab2/lab2/Rational.h
@@ -1,7 +1,5 @@
-//http://www.boost.org/doc/libs/1_56_0/boost/rational.hpp
-//http://www.boost.org/doc/libs/1_56_0/libs/rational/rational.html
-
 #pragma once
+
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 #include <crtdbg.h>
@@ -21,6 +19,55 @@ class Rational {
 public:
 	Tint P, Q;
 
+	Rational(Tint P = 0, Tint Q = 1) :P(P), Q(Q) {
+		Reduce(P, Q);
+	}
+
+	template <typename rat>
+	Rational(Rational<rat> R) : P(R.P), Q(R.Q) {}
+
+	bool operator== (const Rational rhs) const{
+		if (P == rhs.P && Q == rhs.Q) return true;
+		else return false;
+	}
+
+	Rational operator+ (const Rational rhs) const {
+		Tint newP = rhs.Q*P + Q*rhs.P;
+		Tint newQ = Q*rhs.Q;
+		return Rational(newP, newQ);
+	}
+
+	Rational operator+= (const Rational rhs) {
+		P = rhs.Q*P + Q*rhs.P;
+		Q = Q*rhs.Q;
+		Reduce(P, Q);
+		return *this;
+	}
+
+	Rational operator- () {
+		return Rational(-P,Q);
+	}
+
+	// Used as ++Rational
+	Rational operator++ () {
+		P += Q;
+		Reduce(P, Q);
+		return *this;
+	}
+
+
+	// Used as Rational++
+	Rational operator++ (int) {
+		Rational tempRat = *this;
+		P += Q;
+		Reduce(P, Q);
+		return tempRat;
+	}
+
+	explicit operator int () const {
+		return int (P / Q);
+	};
+
 	friend std::ostream& operator<< (std::ostream & cout, Rational<Tint> R){
 		cout << R.P << '/' << R.Q;
 		return cout;
@@ -32,59 +79,4 @@ public:
 		cin >> R.Q;
 		return cin;
 	}
-
-	Rational(Tint P=0, Tint Q=1) :P(P), Q(Q) {
-		Reduce(P, Q);
-	}
-
-	template <typename rat>
-	Rational(Rational<rat> R) : P(R.P), Q(R.Q) {
-
-	}
-
-	bool operator==(const Rational rhs) const{
-		if (P == rhs.P  && Q == rhs.Q){
-			return true;
-		}
-		else
-			return false;
-	}
-
-	Rational operator+(const Rational rhs) const {
-		Tint tempP = rhs.Q*P + Q*rhs.P;
-		Tint tempQ = Q*rhs.Q;
-		return Rational(tempP, tempQ);
-	}
-
-	Rational operator+=(const Rational rhs) {
-		P = rhs.Q*P + Q*rhs.P;
-		Q = Q*rhs.Q;
-		GCD(P, Q);
-		return *this;
-	}
-
-	Rational operator-() {
-		return Rational(-P,Q);
-	}
-
-	/*
-	* Used as ++Rational
-	*/
-	Rational operator++() {
-		P += Q;
-		return *this;
-	}
-
-	/*
-	* Used as Rational++
-	*/
-	Rational operator++(int) {
-		Rational tempRat = *this;
-		P += Q;
-		return tempRat;
-	}
-
-	explicit operator int() const {
-		return int(P / Q);
-	};
 };
